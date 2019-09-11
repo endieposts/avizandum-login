ec72d46755128a61caefd31b3752015796d69f43
endieposts <13526487+endieposts@users.noreply.github.com>
2019-09-11 20:38:07 +0100

    get login working

--- src/main/java/com/endie/avizandum/login/configuration/WebSecurityConfig.java
+++ src/main/java/com/endie/avizandum/login/configuration/WebSecurityConfig.java
@@ -44,12 +44,21 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     }
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
-// We don't need CSRF for this example
+// TODO: put back crsf asap!
         httpSecurity.csrf().disable()
 // dont authenticate this particular request
-                .authorizeRequests().antMatchers("/authenticate").permitAll().
+        .authorizeRequests()
+            .antMatchers("/authenticate").permitAll()
+//            .antMatchers("/", "/home").permitAll()
 // all other requests need to be authenticated
-        anyRequest().authenticated().and().
+        .anyRequest().authenticated().and()
+        .formLogin()
+            .loginPage("/login")
+            .permitAll()
+            .and()
+        .logout()
+            .permitAll()
+            .and().
 // make sure we use stateless session; session won't be used to
 // store user's state.
         exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint).and().sessionManagement()
