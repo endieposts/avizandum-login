356d7ccbffbed29948936315263e1cb9dafb8333
endieposts <13526487+endieposts@users.noreply.github.com>
2019-09-14 15:40:47 +0100

    Set up db each time; insert default user; bCrypt working

--- src/main/java/com/endie/avizandum/login/configuration/WebSecurityConfig.java
+++ src/main/java/com/endie/avizandum/login/configuration/WebSecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -45,6 +46,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+
         auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
     }
 }
