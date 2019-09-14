9de3e9f18518b6fd8cccea8d54cb59a035d3696c
endieposts <13526487+endieposts@users.noreply.github.com>
2019-09-14 20:52:35 +0100

    Replace deprecated validation; lombokify UserRegistrationDto

--- src/main/java/com/endie/avizandum/login/web/dto/UserRegistrationDto.java
+++ src/main/java/com/endie/avizandum/login/web/dto/UserRegistrationDto.java
@@ -1,14 +1,19 @@
 package com.endie.avizandum.login.web.dto;
 
 import com.endie.avizandum.login.constraint.FieldMatch;
-import org.hibernate.validator.constraints.Email;
-import org.hibernate.validator.constraints.NotEmpty;
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.validation.constraints.AssertTrue;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
 
 @FieldMatch.List({
         @FieldMatch(first = "password", second = "confirmPassword", message = "The password fields must match"),
         @FieldMatch(first = "email", second = "confirmEmail", message = "The email fields must match")
 })
+@Getter
+@Setter
 public class UserRegistrationDto {
 
     @NotEmpty
@@ -33,61 +38,5 @@ public class UserRegistrationDto {
 
     @AssertTrue
     private Boolean terms;
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getConfirmPassword() {
-        return confirmPassword;
-    }
-
-    public void setConfirmPassword(String confirmPassword) {
-        this.confirmPassword = confirmPassword;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getConfirmEmail() {
-        return confirmEmail;
-    }
-
-    public void setConfirmEmail(String confirmEmail) {
-        this.confirmEmail = confirmEmail;
-    }
-
-    public Boolean getTerms() {
-        return terms;
-    }
-
-    public void setTerms(Boolean terms) {
-        this.terms = terms;
-    }
 
 }
