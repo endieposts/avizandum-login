9336d3c34e79468f0705e56d40fa8e1ada3584d4
endieposts <13526487+endieposts@users.noreply.github.com>
2019-09-18 20:40:56 +0100

    check session attributes shared by Spring Session amongst instances

--- src/main/java/com/endie/avizandum/login/controller/MainController.java
+++ src/main/java/com/endie/avizandum/login/controller/MainController.java
@@ -3,8 +3,11 @@ package com.endie.avizandum.login.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 @Controller
+@SessionAttributes("testString")
 public class MainController {
 
     @GetMapping("/")
@@ -14,11 +17,16 @@ public class MainController {
 
     @GetMapping("/login")
     public String login(Model model) {
+
+        String testString = "foo";
+
+        model.addAttribute("testString", testString);
+
         return "login";
     }
 
     @GetMapping("/user")
-    public String userIndex() {
+    public String userIndex(@ModelAttribute("testString") String testString) {
         return "index";
     }
 }
